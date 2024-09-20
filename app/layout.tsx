aafee8269202f169a6564ffc773684bd9cd6b2f9
Marlon-S <marlonsuyuc@gmail.com>
2024-09-20 11:44:53 -0600

    delete suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,9 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Navbar />
-          <FilterProvider>
-            <Suspense>{children}</Suspense>
+
+          <FilterProvider >
+            {children}
           </FilterProvider>
           <Footer />
         </Providers>
